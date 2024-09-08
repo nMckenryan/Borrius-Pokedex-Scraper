f44e2751ed44cb87859fd867758cb2e2db8d4042
nMckenryan <n.mckenryan@gmail.com>
2024-09-08 19:27:44 +1000

    Optimisations and parsed stats table

--- scrape_pokemon.py
+++ scrape_pokemon.py
@@ -1,79 +1,68 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-import json
-
 for i in range(1, 494):
-
-    # Search thru all 494 pokemon in the Pokemon Unbound dex
-
-    URL = "https://www.pokemonunboundpokedex.com/borrius/" + str(i)
-    page = requests.get(URL)
-
+    page = requests.get(f"https://www.pokemonunboundpokedex.com/borrius/{i}")
     soup = BeautifulSoup(page.content, "html.parser")
-
-    print("Borrius Pokemon\n==============================\n")
-
-    top_card = soup.find_all(
-        "div", class_="card-body"
-    )
-
-    for p_element in top_card:
-
-        # DEX AND NAMES - first row
-        dex_number = p_element.find("h3", class_="card-title text-5xl").text.strip().replace("Dex Num: ", "")
-        name = p_element.find("h3", class_="card-title text-4xl").text.strip().replace("Name: ", "")
-        
-        first_card_table = p_element.find("div", class_="grid grid-cols-3")
-        
-        # Type, Catchrate, Gender, Abilities, Weight, Height
-        statBoxes = first_card_table.find_all(
-            "p", class_="text-3xl font-bold", limit=6
-        )
-
-        
-        type = statBoxes[0].text.strip()
-        catchRate = statBoxes[1].text.strip()
-        gender = statBoxes[2].text.strip()
-        abilities = statBoxes[3].text.strip()
-        
-        weight_split = statBoxes[4].text.strip().split(" ")
-        
-        weight_metric = weight_split[0].replace("\u00a0", "")
-        weight_imperial = weight_split[1].replace("\u00a0", "").replace('(', '').replace(')', '')
-        
-        height_split = statBoxes[5].text.strip().split(" ")
-
-        height_metric = height_split[0].replace("\u00a0", "")
-        height_imperial = height_split[1].replace("\u2032", "'").replace("\u2033", "'").replace('(', '').replace(')', '')
-        
-        
-       
-        pokemon_data = {
-            "pokemon_index": dex_number,
-            "name": name,
-            "type": type,
-            "catchRate": catchRate,
-            "gender": gender,
-            "abilities": abilities,
-            "weight": {
-                "imperial": weight_imperial,
-                "metric": weight_metric,
-            },
-            "height": {
-                "imperial": height_imperial,
-                "metric": height_metric,
-            },
-        }
-        
-        # Convert the dictionary to JSON
-        pokemon_json = json.dumps(pokemon_data, indent=4)   
-
-        # Print the JSON output
-        print(pokemon_json)
-        
-
+    top_card = soup.find("div", class_="card-body")
     
-        
-       
+    
+    stats_table = soup.find("table", class_="table table-zebra");
+    
+    
+    
+    
+    stats_table = {
+        "hp": {
+            "avg": int(stats_table.find_all("td")[0].text.strip()),
+            "min": int(stats_table.find_all("td")[1].text.strip()),
+            "max": int(stats_table.find_all("td")[2].text.strip()),
+        }, 
+        "attack": {
+            "avg": int(stats_table.find_all("td")[3].text.strip()),
+            "min": int(stats_table.find_all("td")[4].text.strip()),
+            "max": int(stats_table.find_all("td")[5].text.strip()),
+        }, 
+        "defense": {
+            "avg": int(stats_table.find_all("td")[6].text.strip()),
+            "min": int(stats_table.find_all("td")[7].text.strip()),
+            "max": int(stats_table.find_all("td")[8].text.strip()),
+        }, 
+        "specialAttack": {
+            "avg": int(stats_table.find_all("td")[9].text.strip()),
+            "min": int(stats_table.find_all("td")[10].text.strip()),
+            "max": int(stats_table.find_all("td")[11].text.strip()),
+        }, 
+        "specialDefense": {
+            "avg": int(stats_table.find_all("td")[12].text.strip()),
+            "min": int(stats_table.find_all("td")[13].text.strip()),
+            "max": int(stats_table.find_all("td")[14].text.strip()),
+        },
+        "speed": {
+            "avg": int(stats_table.find_all("td")[15].text.strip()),
+            "min": int(stats_table.find_all("td")[16].text.strip()),
+            "max": int(stats_table.find_all("td")[17].text.strip()),
+        }
+    }
+    
+    
+    pokemon_data = {
+        "pokemon_index": top_card.find("h3", class_="card-title text-5xl").text.strip().replace("Dex Num: ", ""),
+        "name": top_card.find("h3", class_="card-title text-4xl").text.strip().replace("Name: ", ""),
+        "type": top_card.find_all("p", class_="text-3xl font-bold")[0].text.strip(),
+        "catchRate": top_card.find_all("p", class_="text-3xl font-bold")[1].text.strip(),
+        "gender": top_card.find_all("p", class_="text-3xl font-bold")[2].text.strip(),
+        "abilities": top_card.find_all("p", class_="text-3xl font-bold")[3].text.strip(),
+        "weight": {
+            "imperial": top_card.find_all("p", class_="text-3xl font-bold")[4].text.strip().split(" ")[1].replace("\u00a0", "").replace('(', '').replace(')', ''),
+            "metric": top_card.find_all("p", class_="text-3xl font-bold")[4].text.strip().split(" ")[0].replace("\u00a0", ""),
+        },
+        "height": {
+            "imperial": top_card.find_all("p", class_="text-3xl font-bold")[5].text.strip().split(" ")[1].replace("\u2032", "'").replace("\u2033", "'").replace('(', '').replace(')', ''),
+            "metric": top_card.find_all("p", class_="text-3xl font-bold")[5].text.strip().split(" ")[0].replace("\u00a0", ""),
+        },
+        "stats": stats_table
+    }
+    print(json.dumps(pokemon_data, indent=4))
 
