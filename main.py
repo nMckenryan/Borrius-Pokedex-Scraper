2633459bc95781f62c4e5ad362a7592183f0c49c
nMckenryan <n.mckenryan@gmail.com>
2024-09-10 13:45:41 +1000

    Finished get individual pokemon endpoint

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ app = FastAPI()
 
 
 @app.get("/borrius_pokemon/info")
-async def read_pokemon():
+async def read_info():
     with open("borrius_pokedex_data.json") as json_file:
         pokemon_data = json.load(json_file)[0].get("info")
     return {"borrius_pokemon": pokemon_data}
 
 
-@app.get("/borrius_pokemon/all")
+@app.get("/borrius_pokemon/")
 async def read_pokemon():
     with open("borrius_pokedex_data.json") as json_file:
         pokemon_data = json.load(json_file)[0].get("pokemon")
     return {"borrius_pokemon": pokemon_data}
+
+
+@app.get("/borrius_pokemon/{pokemon_id}")
+async def read_pokemon(pokemon_id: int):
+    with open("borrius_pokedex_data.json") as json_file:
+        pokemon_data = json.load(json_file)[0].get("pokemon")
+    try:
+        return {"borrius_pokemon": pokemon_data[pokemon_id - 1]}
+    except IndexError:
+        return {"error": "Invalid pokemon ID"}
