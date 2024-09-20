9e682ef8371743a9ad33c8acdc9a5cf03e803169
nMckenryan <n.mckenryan@gmail.com>
2024-09-20 15:00:41 +1000

    marked special encounters

--- borrius_location_scraper.py
+++ borrius_location_scraper.py
@@ -10,33 +10,40 @@ locationDataList = []
 # map through each column of the spreadsheet, apply the headers as locationData.location
 for col in range(1, sheet.max_column + 1):
     location_header = sheet.cell(row=1, column=col).value
+    isSpecialEncounter = 0
     # map down through each row, applying each value from there as "pokemon"
     for row in range(2, sheet.max_row + 1):
         pokemon = sheet.cell(row=row, column=col).value
-        if pokemon is not None:
-            match sheet.cell(row=row, column=col).fill.start_color.rgb:
-                case "ffb500":
-                    timeOfDay = "Morning"
-                case "b300ff":
-                    timeOfDay = "Night"
-                case "ff0000":
-                    timeOfDay = "Swarm"
-                case "00ff00":
-                    timeOfDay = "Special Encounter"
-                case _:
-                    timeOfDay = "All Day"
-            locationDataList.append(
-                {
-                    "pokemon": pokemon,
-                    "locationData": [
-                        {
-                            "location": location_header,
-                            "encounterMethod": "Grass" if col < 5 else "Cave",
-                            "timeOfDay": timeOfDay,
-                        }
-                    ],
-                }
-            )
+        if pokemon is None:
+            continue
+        # Special encounters are shown at the bottom of the row.
+        if pokemon == "Special Encounter":
+            isSpecialEncounter = 1
+            continue
+
+        match sheet.cell(row=row, column=col).fill.start_color.rgb:
+            case "ffb500":
+                timeOfDay = "Morning"
+            case "b300ff":
+                timeOfDay = "Night"
+            case "ff0000":
+                timeOfDay = "Swarm"
+            case _:
+                timeOfDay = "All Day"
+
+        locationDataList.append(
+            {
+                "pokemon": pokemon,
+                "locationData": [
+                    {
+                        "location": location_header,
+                        "encounterMethod": "Grass" if col < 5 else "Cave",
+                        "timeOfDay": timeOfDay,
+                        "isSpecialEncounter": isSpecialEncounter,
+                    }
+                ],
+            }
+        )
 
 try:
     fileName = "locationData.json"
