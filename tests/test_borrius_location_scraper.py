584cfbeb37d38a9fc54543bbe6d5da2f813d10c9
nMckenryan <n.mckenryan@gmail.com>
2024-10-19 21:37:38 +1100

    Repaired tests after failed upgrade to Poetry

--- tests/test_borrius_location_scraper.py
+++ tests/test_borrius_location_scraper.py
@@ -1,9 +1,10 @@
 import pytest
-from borrius_location_scraper import get_grasscave_locations, get_fishing_locations,\
+from borrius_location_scraper import get_grasscave_locations, get_fishing_locations, \
     get_surf_locations, fill_in_evolution_gaps, print_json_file, print_location_json
 from unittest.mock import patch, mock_open
 import json
 from unittest import mock
+
 
 @pytest.mark.asyncio
 async def test_get_grasscave_locations():
@@ -26,7 +27,7 @@ async def test_get_fishing_locations():
     assert len(locationDataList) == 75
     assert locationDataList[0]["pokemon"] == "shellder"
     assert len(locationDataList[0]["locationData"]) == 27
-    assert locationDataList[0]["locationData"][0] == {'location': 'Route 2', 'encounterMethod': 'Good Rod',  'timeOfDay': 'All Day', 'isSpecialEncounter': 0}
+    assert locationDataList[0]["locationData"][0] == {'location': 'Route 2', 'encounterMethod': 'Good Rod', 'timeOfDay': 'All Day', 'isSpecialEncounter': 0}
     
     
 @pytest.mark.asyncio
@@ -39,6 +40,7 @@ async def test_get_surf_locations():
     assert locationDataList[0]["pokemon"] == "Tentacool"
     assert len(locationDataList[0]["locationData"]) == 23
     assert locationDataList[0]["locationData"][0] == {'location': 'Route 2', 'encounterMethod': 'Surfing', 'timeOfDay': 'All Day', 'isSpecialEncounter': 0}
+
     
 @pytest.mark.asyncio
 async def test_fill_in_evolution_gaps():
@@ -49,10 +51,6 @@ async def test_fill_in_evolution_gaps():
 
     assert len(locationDataList[0]["locationData"]) == 1
     assert locationDataList[0]["locationData"][0] == {'location': 'Evolve Larvitar (Lv. 30)', 'encounterMethod': 'Evolution', 'timeOfDay': 'All Day', 'isSpecialEncounter': 0}
-
-
-
-
 
 # @pytest.mark.asyncio
 # async def test_print_location_json(tmp_path, mocker):
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,12 +1,11 @@
 import aiohttp
 import pytest
-from helpers import get_pokemon_index_from_name, get_pokemon_indexes_from_list, get_pokemon_names_from_unbound_pokedex, read_location_data_json, correct_pokemon_name, fetch_page,\
-    get_pokemon_locations, get_evolution_data_from_pokeapi,\
-    initialise_pokemon_location_template, get_special_encounter_pokemon,\
+from helpers import get_pokemon_index_from_name, get_pokemon_indexes_from_list, get_pokemon_names_from_unbound_pokedex, read_location_data_json, correct_pokemon_name, fetch_page, \
+    get_pokemon_locations, get_evolution_data_from_pokeapi, \
+    initialise_pokemon_location_template, get_special_encounter_pokemon, \
     get_pokemon_api_data_gaps, get_missing_pokemon_data
 from unittest.mock import patch, mock_open
 import json
-
 
 
 @pytest.mark.asyncio
@@ -25,12 +24,14 @@ async def test_read_location_data_json_file_not_found():
         result = await read_location_data_json()
         assert result == [] or result == None
 
+
 @pytest.mark.asyncio
 async def test_read_location_data_json_json_decode_error():
     with patch("builtins.open", mock_open(read_data="not a json")):
         with patch("json.load", side_effect=json.JSONDecodeError("Expecting value", "", 0)):
             result = await read_location_data_json()
             assert result == [] or result == None
+
 
 @pytest.mark.asyncio
 async def test_read_location_data_json_success():
@@ -39,6 +40,7 @@ async def test_read_location_data_json_success():
         with patch("json.load", return_value=mock_data):
             result = await read_location_data_json()
             assert result == mock_data
+
 
 @pytest.mark.asyncio
 async def test_check_for_forbidden_names():
@@ -52,11 +54,12 @@ async def test_check_for_forbidden_names():
         assert pokemon["pokemon"] != "old rod"
         assert pokemon["pokemon"] != "good rod"
 
+
 @pytest.mark.asyncio
 def test_get_pokemon_locations():
     location_list = [{"pokemon": "snorunt", "locationData": [{"location": "Route 1"}, {"location": "Route 8"}]},
                      {"pokemon": "glacie", "locationData": []}]
-    pokemon_location =  get_pokemon_locations("snorunt", location_list)
+    pokemon_location = get_pokemon_locations("snorunt", location_list)
     
     assert len(pokemon_location) == 2
     assert pokemon_location[0]['location'] == 'Route 1'
@@ -69,6 +72,7 @@ async def test_get_evolution_data_from_pokeapi():
     result = await get_evolution_data_from_pokeapi(pokemon_number)
     assert 'evolution_details' in result
 
+
 @pytest.mark.asyncio
 async def test_correctPokemonName():
     result = correct_pokemon_name("minior")
@@ -79,11 +83,13 @@ async def test_correctPokemonName():
 async def test_correctPokemonName_fossil():
     result = correct_pokemon_name("Dome Fossil")
     assert result == "kabuto"
+
     
 @pytest.mark.asyncio
 async def test_correctPokemonName_regional():
     result = correct_pokemon_name("galarian numel")
     assert result == "numel-galar"
+
 
 @pytest.mark.asyncio
 async def test_initialise_pokemon_location_template():
@@ -97,8 +103,9 @@ async def test_initialise_pokemon_location_template():
 
 @pytest.mark.asyncio
 def test_get_special_encounter_pokemon():
-    result =  get_special_encounter_pokemon()
+    result = get_special_encounter_pokemon()
     assert len(result) > 0
+
 
 @pytest.mark.asyncio
 async def test_get_pokemon_api_data_gaps():
@@ -124,6 +131,7 @@ async def test_get_pokemon_names_from_unbound_pokedex():
 async def test_get_pokemon_index_from_name():
     pokemon_index = await get_pokemon_index_from_name("minior")
     assert pokemon_index == 774
+
     
 @pytest.mark.asyncio
 async def test_get_pokemon_indexes_from_list():
